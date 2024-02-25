f09bd7cb77a0a698127b7f4234a46f14769cbdec
ArthurCosmix <158165446+ArthurCosmix@users.noreply.github.com>
2024-02-25 21:08:51 +0700

    add drawFeature but can't change resolution

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const GRIDSIDE = 600
-const squarePerSide = 16
+const squarePerSide = 50
+//Get element from html element 
+const gridButton = document.querySelector(".grid-button")
+const resolutionBar = document.querySelector("#sketch-resolution")
+let resolutionValue = document.querySelector("#sketch-resolution").value
+const resolutionStatus = document.querySelector(".resolution-status")
+
+function showResolution(){
+    resolutionStatus.textContent = `${resolutionValue} × ${resolutionValue} (Resolution) `
+}
+
+
 
 const sketchBoard = document.querySelector("#sketch-board")
 sketchBoard.style.width = sketchBoard.style.height = `${GRIDSIDE}px`
 
-function createSketchBoard(){
+//function drawOnSketch
+function drawOnSketch(){
+    this.style.backgroundColor = "black"
+}
+
+function createSketchBoard(squarePerSide){
     for(let i = 0; i < (squarePerSide * squarePerSide); i++){
         const gridCell = document.createElement("div")
         gridCell.style.width = gridCell.style.height = `${(GRIDSIDE / squarePerSide)}px`
         gridCell.classList.add("cell")
         sketchBoard.appendChild(gridCell)
-        
+        gridCell.addEventListener("mouseover", drawOnSketch)
     }
+    }
+  
+
+function run(){
+    createSketchBoard(squarePerSide)
 }
-createSketchBoard()
+run()
